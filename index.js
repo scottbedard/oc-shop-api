177c25b43794beb832d188a86d74367726f5d81c
scottbedard <scottbedard90@gmail.com>
2017-04-01 11:04:26 -0700

    Add category methods

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log ('soon...');
+var axios = require('axios');
+
+module.exports = {
+
+    /**
+     * API Host
+     *
+     * @type {String}
+     */
+    host: '',
+
+    /**
+     * Find a category by slug.
+     *
+     * @param  {string}     slug
+     * @return {Promise}
+     */
+    findCategory: function(slug) {
+        return axios.get(this.host + '/api/bedard/shop/categories/' + slug)
+    },
+
+    /**
+     * Get categories.
+     *
+     * @param  {Object}     params
+     * @return {Promise}
+     */
+    getCategories: function(params) {
+        return axios.get(this.host + '/api/bedard/shop/categories', { params });
+    },
+};
